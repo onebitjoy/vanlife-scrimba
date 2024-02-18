d236374af327c9cb7c82746e55efe76e8aa723f6
abhishek <95994085+onebitjoy@users.noreply.github.com>
2024-02-18 19:39:49 +0530

    Chore: Deleted dist folder, Style: Added Navigation style

--- pages/Host/HostNavBar.jsx
+++ pages/Host/HostNavBar.jsx
@@ -1,14 +1,17 @@
-import { Link } from "react-router-dom"
+// import { Link, NavLink } from "react-router-dom"
+
+import { NavLink } from "react-router-dom"
 import "./hostnavbar.css"
 
 function HostNavBar() {
+
   return (
     <nav className="hostNavBar">
-      <Link className="hostNavBarLink" to="/host">Dashboard</Link>
-      <Link className="hostNavBarLink" to="/host/income">Income</Link>
-      <Link className="hostNavBarLink" to="/host/vans">Vans</Link>
-      <Link className="hostNavBarLink" to="/host/reviews">Reviews</Link>
-    </nav>
+      <NavLink end className="hostNavBarLink" to="/host">Dashboard</NavLink>
+      <NavLink className="hostNavBarLink" to="/host/income">Income</NavLink>
+      <NavLink className="hostNavBarLink" to="/host/vans">Vans</NavLink>
+      <NavLink className="hostNavBarLink" to="/host/reviews">Reviews</NavLink >
+    </nav >
   )
 }
 
